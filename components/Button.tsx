1e36c63074569f8aaf671449275c06a0763e64c6
Anmitchell <avarynmitchell@gmail.com>
2024-10-29 21:35:46 -0400

    User can can emoji's to images

--- components/Button.tsx
+++ components/Button.tsx
@@ -32,15 +32,11 @@ export default function Button({ label, theme, onPress }: Props) {
         </Pressable>
       </View>
     );
-    /* @tutinfo */
   }
 
   return (
     <View style={styles.buttonContainer}>
-      <Pressable
-        style={styles.button}
-        onPress={() => alert('You pressed a button.')}
-      >
+      <Pressable style={styles.button} onPress={onPress}>
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
